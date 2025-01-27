d387c3b1217dcb4ad4f3b570557422f651c85b74
Astridisbach <astridisbachsorensen@gmail.com>
2025-01-27 09:35:28 +0100

    opgave 2

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//variabler for navn og alder
+let name = `Astrid`;
+let age = 23;
+
+//viser beskeden
+console.log(`Hi ${name}, you are ${age} years old!`);
+
+//variablerne med nye værdier
+name = `Signe`;
+age = 25;
+
+//pdaterede besked
+console.log(`Hi ${name}, you are ${age} years old!`);
